8004855416719eccbd1f7dc3c71db26775af585f
Kalpesh Choudhary <kalpesh@retire100.live>
2023-07-03 12:27:20 +0530

    ui fix

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
@@ -9,16 +10,18 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Select a Category'),
+        title: const Text('Pick your category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio: 3/2,
+          childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
-          mainAxisExtent: 20,
+          mainAxisSpacing: 20,
         ),
         children: [
+          // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
           for (final category in availableCategories)
             CategoryGridItem(category: category)
         ],
