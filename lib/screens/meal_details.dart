3c08488ce92fd0a91bc7fa51c3499b3ae04d0d91
Kalpesh Choudhary <kalpesh@retire100.live>
2023-07-10 13:45:06 +0530

    meal detail page

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealDetailsScreen extends StatelessWidget {
+  const MealDetailsScreen({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: Column(
+        children: [
+          Image.network(
+            meal.imageUrl,
+            width: double.infinity,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 14,),
+          Text('Ingredients',style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -4,14 +4,16 @@ import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectMeal});
 
   final Meal meal;
-  
+  final void Function(Meal meal) onSelectMeal;
+
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() +
         meal.complexity.name.substring(1);
   }
+
   String get affordabilityText {
     return meal.affordability.name[0].toUpperCase() +
         meal.affordability.name.substring(1);
@@ -25,7 +27,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 5,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
@@ -68,16 +72,16 @@ class MealItem extends StatelessWidget {
                         const SizedBox(
                           width: 12,
                         ),
-                        MealItemTrait(
-                            icon: Icons.work, label: complexityText),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
                         const SizedBox(
                           width: 12,
                         ),
                         MealItemTrait(
                             icon: Icons.attach_money, label: affordabilityText),
                       ],
-                    ),const SizedBox(
-                      height:3,
+                    ),
+                    const SizedBox(
+                      height: 3,
                     ),
                   ],
                 ),
